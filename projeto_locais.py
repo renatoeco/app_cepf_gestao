d2fbd9e5ade7499522800d15ee1bc2e773d9b04e
renatoeco <renato.eco@gmail.com>
2026-01-11 16:43:00 -0300

    Remoção de função duplicada em locais.

--- projeto_locais.py
+++ projeto_locais.py
@@ -98,20 +98,20 @@ col_kbas = db["kbas"]
 ###########################################################################################################
 
 
-# Escopo mínimo necessário para Drive
-ESCOPO_DRIVE = ["https://www.googleapis.com/auth/drive"]
+# # Escopo mínimo necessário para Drive
+# ESCOPO_DRIVE = ["https://www.googleapis.com/auth/drive"]
 
-@st.cache_resource
-def obter_servico_drive():
-    """
-    Retorna o cliente autenticado do Google Drive,
-    usando as credenciais armazenadas em st.secrets.
-    """
-    credenciais = Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=ESCOPO_DRIVE
-    )
-    return build("drive", "v3", credentials=credenciais)
+# @st.cache_resource
+# def obter_servico_drive():
+#     """
+#     Retorna o cliente autenticado do Google Drive,
+#     usando as credenciais armazenadas em st.secrets.
+#     """
+#     credenciais = Credentials.from_service_account_info(
+#         st.secrets["gcp_service_account"],
+#         scopes=ESCOPO_DRIVE
+#     )
+#     return build("drive", "v3", credentials=credenciais)
 
 
 
