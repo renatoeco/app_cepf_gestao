f120f8df3842ede6b3f4961323c777e3e3a2f39b
renatoeco <renato.eco@gmail.com>
2026-03-07 10:11:19 -0300

    Adaptação de projeto_novo para nova estrutura de direções estratégicas dentro do edital.

--- projeto_novo.py
+++ projeto_novo.py
@@ -14,11 +14,9 @@ col_projetos = db["projetos"]
 col_pessoas = db["pessoas"]
 col_organizacoes = db["organizacoes"]
 col_editais = db["editais"]
-col_direcoes = db["direcoes_estrategicas"]
 col_publicos = db["publicos"]
 
 df_pessoas = pd.DataFrame(list(col_pessoas.find()))
-df_direcoes = pd.DataFrame(list(col_direcoes.find()))
 df_publicos = pd.DataFrame(list(col_publicos.find()))
 
 ###########################################################################################################
@@ -91,20 +89,34 @@ lista_ids = list(mapa_id_nome.keys())
 
 
 ###########################################################################################################
+# EDITAL (FORA DO FORM)
+###########################################################################################################
+
+editais = list(col_editais.find().sort("data_lancamento", -1))
+lista_editais = [e["codigo_edital"] for e in editais]
+
+edital = st.selectbox(
+    "Edital",
+    lista_editais,
+    index=lista_editais.index(st.session_state.form_projeto["edital"])
+    if st.session_state.form_projeto["edital"] in lista_editais else 0
+)
+
+# pega o documento do edital selecionado
+edital_doc = next((e for e in editais if e["codigo_edital"] == edital), {})
+
+
+direcoes_edital = [
+    d["tema"] for d in edital_doc.get("direcoes_estrategicas", [])
+]
+
+
+
+###########################################################################################################
 # FORMULÁRIO
 ###########################################################################################################
 
 with st.form(key=f"form_novo_projeto_{st.session_state.form_key}", border=False):
-
-    editais = list(col_editais.find().sort("data_lancamento", -1))
-    lista_editais = [e["codigo_edital"] for e in editais]
-
-    edital = st.selectbox(
-        "Edital",
-        lista_editais,
-        index=lista_editais.index(st.session_state.form_projeto["edital"])
-        if st.session_state.form_projeto["edital"] in lista_editais else 0
-    )
 
     orgs = list(col_organizacoes.find().sort("nome_organizacao", 1))
     lista_orgs = [o["nome_organizacao"] for o in orgs]
@@ -150,7 +162,6 @@ with st.form(key=f"form_novo_projeto_{st.session_state.form_key}", border=False)
         format="DD/MM/YYYY"
     )
 
-
     responsaveis_ids = st.multiselect(
         "Responsáveis",
         lista_ids,
@@ -158,12 +169,20 @@ with st.form(key=f"form_novo_projeto_{st.session_state.form_key}", border=False)
         format_func=lambda x: "" if x is None else mapa_id_nome[x]
     )
 
+    #######################################################################################################
+    # DIREÇÕES ESTRATÉGICAS
+    #######################################################################################################
 
     direcoes = st.multiselect(
         "Direções estratégicas",
-        df_direcoes["tema"].tolist(),
-        default=st.session_state.form_projeto["direcoes"]
+        direcoes_edital,
+        default=[
+            d for d in st.session_state.form_projeto["direcoes"]
+            if d in direcoes_edital
+        ]
     )
+
+    #######################################################################################################
 
     publicos = st.multiselect(
         "Públicos",
@@ -177,6 +196,14 @@ with st.form(key=f"form_novo_projeto_{st.session_state.form_key}", border=False)
     )
 
     submit = st.form_submit_button("Cadastrar projeto", type="primary")
+
+
+
+
+
+
+
+
 
 
 ###########################################################################################################
@@ -305,350 +332,5 @@ if submit:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ###########################################################################################################
-# # FORMULÁRIO
-# ###########################################################################################################
-
-# with st.form(key=f"form_novo_projeto_{st.session_state.form_key}", border=False):
-
-#     # EDITAL
-#     editais = list(col_editais.find().sort("data_lancamento", -1))
-#     lista_editais = [e["codigo_edital"] for e in editais]
-
-#     edital = st.selectbox(
-#         "Edital",
-#         lista_editais,
-#         index=lista_editais.index(st.session_state.form_projeto["edital"])
-#         if st.session_state.form_projeto["edital"] in lista_editais else 0
-#     )
-
-#     # ORGANIZAÇÃO
-#     orgs = list(col_organizacoes.find().sort("nome_organizacao", 1))
-#     lista_orgs = [o["nome_organizacao"] for o in orgs]
-
-#     organizacao = st.selectbox(
-#         "Organização",
-#         lista_orgs,
-#         index=lista_orgs.index(st.session_state.form_projeto["organizacao"])
-#         if st.session_state.form_projeto["organizacao"] in lista_orgs else 0
-#     )
-
-#     codigo_projeto = st.text_input(
-#         "Código do Projeto",
-#         value=st.session_state.form_projeto["codigo"]
-#     )
-
-#     sigla_projeto = st.text_input(
-#         "Sigla do Projeto",
-#         value=st.session_state.form_projeto["sigla"]
-#     )
-
-#     nome_projeto = st.text_input(
-#         "Nome do Projeto",
-#         value=st.session_state.form_projeto["nome"]
-#     )
-
-#     duracao = st.number_input(
-#         "Duração do Projeto (meses)",
-#         min_value=1,
-#         step=1,
-#         value=st.session_state.form_projeto["duracao"]
-#     )
-
-#     data_inicio = st.date_input(
-#         "Data de Início",
-#         value=st.session_state.form_projeto["data_inicio"],
-#         format="DD/MM/YYYY"
-#     )
-
-#     data_fim = st.date_input(
-#         "Data de Fim",
-#         value=st.session_state.form_projeto["data_fim"],
-#         format="DD/MM/YYYY"
-#     )
-
-
-#     responsaveis = df_pessoas[df_pessoas["tipo_usuario"].str.contains("beneficiario", na=False)]["nome_completo"].tolist()
-#     responsaveis.insert(0, "")
-
-
-#     # cria lista de beneficiários mantendo id + nome
-#     pessoas_benef = df_pessoas[
-#         df_pessoas["tipo_usuario"].str.contains("beneficiario", na=False)
-#     ][["_id", "nome_completo"]]
-
-#     # dicionário: nome -> _id
-#     mapa_responsaveis = {
-#         row["nome_completo"]: row["_id"]
-#         for _, row in pessoas_benef.iterrows()
-#     }
-
-#     lista_nomes = [""] + list(mapa_responsaveis.keys())
-
-#     responsavel_nome = st.selectbox(
-#         "Responsável",
-#         lista_nomes,
-#         index=0
-#     )
-
-
-
-#     # responsavel = st.selectbox(
-#     #     "Responsável",
-#     #     responsaveis,
-#     #     index=responsaveis.index(st.session_state.form_projeto["responsavel"])
-#     #     if st.session_state.form_projeto["responsavel"] in responsaveis else 0
-#     # )
-
-#     direcoes = st.multiselect(
-#         "Direções estratégicas",
-#         df_direcoes["tema"].tolist(),
-#         default=st.session_state.form_projeto["direcoes"]
-#     )
-
-#     publicos = st.multiselect(
-#         "Públicos",
-#         df_publicos["publico"].tolist(),
-#         default=st.session_state.form_projeto["publicos"]
-#     )
-
-#     objetivo = st.text_area(
-#         "Objetivo geral",
-#         value=st.session_state.form_projeto["objetivo"]
-#     )
-
-#     submit = st.form_submit_button("Cadastrar projeto", type="primary")
-
-
-
-
-# ###########################################################################################################
-# # PROCESSAMENTO
-# ###########################################################################################################
-
-# if submit:
-
-#     # Salva valores atuais no session_state
-#     st.session_state.form_projeto.update({
-#         "edital": edital,
-#         "organizacao": organizacao,
-#         "codigo": codigo_projeto,
-#         "sigla": sigla_projeto,
-#         "nome": nome_projeto,
-#         "duracao": duracao,
-#         "data_inicio": data_inicio,
-#         "data_fim": data_fim,
-#         "responsavel": responsavel,
-#         "direcoes": direcoes,
-#         "publicos": publicos,
-#         "objetivo": objetivo
-#     })
-
-#     # Validação
-#     obrigatorios = {
-#         "Edital": edital,
-#         "Código": codigo_projeto,
-#         "Sigla": sigla_projeto,
-#         "Nome": nome_projeto,
-#         "Objetivo": objetivo,
-#         "Data início": data_inicio,
-#         "Data fim": data_fim,
-#         "Direções": direcoes,
-#         "Públicos": publicos
-#     }
-
-#     faltando = [k for k, v in obrigatorios.items() if not v]
-
-#     if faltando:
-#         st.error(f"Preencha os campos obrigatórios: {', '.join(faltando)}")
-
-#     elif col_projetos.find_one({"codigo": codigo_projeto}):
-#         st.warning(
-#             f":material/warning: Já existe um projeto com o código {codigo_projeto}. O projeto não foi cadastrado."
-#         )
-
-#     elif col_projetos.find_one({"sigla": sigla_projeto}):
-#         st.warning(
-#             f":material/warning: Já existe um projeto com a sigla {sigla_projeto}. O projeto não foi cadastrado."
-#         )
-
-#     else:
-#         ###################################################################################################
-#         # 1) INSERE O PROJETO (SEM RESPONSÁVEL)
-#         ###################################################################################################
-
-#         col_projetos.insert_one({
-#             "_id": bson.ObjectId(),
-#             "edital": edital,
-#             "codigo": codigo_projeto,
-#             "sigla": sigla_projeto,
-#             "organizacao": organizacao,
-#             "nome_do_projeto": nome_projeto,
-#             "objetivo_geral": objetivo,
-#             "duracao": duracao,
-#             "data_inicio_contrato": data_inicio.strftime("%d/%m/%Y"),
-#             "data_fim_contrato": data_fim.strftime("%d/%m/%Y"),
-#             "direcoes_estrategicas": direcoes,
-#             "publicos": publicos,
-#             "status": "Em dia"
-#         })
-
-#         ###################################################################################################
-#         # 2) ATUALIZA A PESSOA RESPONSÁVEL (ADICIONA O CÓDIGO NA LISTA projetos[])
-#         ###################################################################################################
-#         # Usa $addToSet para evitar duplicação automática
-
-
-#         if responsavel_nome:
-#             responsavel_id = mapa_responsaveis[responsavel_nome]
-
-#             col_pessoas.update_one(
-#                 {"_id": responsavel_id},  # agora é único e seguro
-#                 {
-#                     "$addToSet": {
-#                         "projetos": codigo_projeto
-#                     }
-#                 }
-#             )
-
-
-
-#         # if responsavel:
-#         #     col_pessoas.update_one(
-#         #         {"nome_completo": responsavel},
-#         #         {
-#         #             "$addToSet": {
-#         #                 "projetos": codigo_projeto
-#         #             }
-#         #         }
-#         #     )
-
-#         st.success("Projeto cadastrado com sucesso!", icon=":material/check:")
-
-#         ###################################################################################################
-#         # RESET TOTAL DO FORMULÁRIO (mantido igual ao seu)
-#         ###################################################################################################
-
-#         st.session_state.form_projeto = {
-#             "edital": "",
-#             "organizacao": "",
-#             "codigo": "",
-#             "sigla": "",
-#             "nome": "",
-#             "duracao": 1,
-#             "data_inicio": None,
-#             "data_fim": None,
-#             "responsavel": "",
-#             "direcoes": [],
-#             "publicos": [],
-#             "objetivo": ""
-#         }
-
-#         st.session_state.form_key += 1
-#         time.sleep(3)
-#         st.rerun()
-
-
-
-
-# # ###########################################################################################################
-# # # PROCESSAMENTO
-# # ###########################################################################################################
-
-# # if submit:
-
-# #     # Salva valores atuais
-# #     st.session_state.form_projeto.update({
-# #         "edital": edital,
-# #         "organizacao": organizacao,
-# #         "codigo": codigo_projeto,
-# #         "sigla": sigla_projeto,
-# #         "nome": nome_projeto,
-# #         "duracao": duracao,
-# #         "data_inicio": data_inicio,
-# #         "data_fim": data_fim,
-# #         "responsavel": responsavel,
-# #         "direcoes": direcoes,
-# #         "publicos": publicos,
-# #         "objetivo": objetivo
-# #     })
-
-# #     # Validação
-# #     obrigatorios = {
-# #         "Edital": edital,
-# #         "Código": codigo_projeto,
-# #         "Sigla": sigla_projeto,
-# #         "Nome": nome_projeto,
-# #         "Objetivo": objetivo,
-# #         "Data início": data_inicio,
-# #         "Data fim": data_fim,
-# #         "Direções": direcoes,
-# #         "Públicos": publicos
-# #     }
-
-# #     faltando = [k for k, v in obrigatorios.items() if not v]
-
-# #     if faltando:
-# #         st.error(f"Preencha os campos obrigatórios: {', '.join(faltando)}")
-
-# #     elif col_projetos.find_one({"codigo": codigo_projeto}):
-# #         st.warning(f":material/warning: Já existe um projeto com o código {codigo_projeto}. O projeto não foi cadastrado.")
-
-# #     elif col_projetos.find_one({"sigla": sigla_projeto}):
-# #         st.warning(f":material/warning: Já existe um projeto com a sigla {sigla_projeto}. O projeto não foi cadastrado.")
-
-# #     else:
-# #         col_projetos.insert_one({
-# #             "_id": bson.ObjectId(),
-# #             "edital": edital,
-# #             "codigo": codigo_projeto,
-# #             "sigla": sigla_projeto,
-# #             "organizacao": organizacao,
-# #             "nome_do_projeto": nome_projeto,
-# #             "objetivo_geral": objetivo,
-# #             "duracao": duracao,
-# #             "data_inicio_contrato": data_inicio.strftime("%d/%m/%Y"),
-# #             "data_fim_contrato": data_fim.strftime("%d/%m/%Y"),
-# #             "responsavel": responsavel,
-# #             "direcoes_estrategicas": direcoes,
-# #             "publicos": publicos,
-# #             "status": "Em dia"
-# #         })
-
-# #         st.success("Projeto cadastrado com sucesso!")
-
-# #         # RESET TOTAL DO FORMULÁRIO
-# #         st.session_state.form_projeto = {
-# #             "edital": "",
-# #             "organizacao": "",
-# #             "codigo": "",
-# #             "sigla": "",
-# #             "nome": "",
-# #             "duracao": 1,
-# #             "data_inicio": None,
-# #             "data_fim": None,
-# #             "responsavel": "",
-# #             "direcoes": [],
-# #             "publicos": [],
-# #             "objetivo": ""
-# #         }
-
-# #         # força recriação do formulário
-# #         st.session_state.form_key += 1
-# #         time.sleep(3)
-# #         st.rerun()
 
 
