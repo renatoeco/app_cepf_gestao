96e60e9283e82107288eba5f80b748732dd96f08
renatoeco <renato.eco@gmail.com>
2026-03-25 10:19:56 -0300

    Ajuste nas mensagens de atividade atrasada na home do projeto.

--- projeto_visao_geral.py
+++ projeto_visao_geral.py
@@ -696,63 +696,66 @@ if not editar_cadastro:
     # ATIVIDADES ATRASADAS
     ###########################################################################################################
 
-    # Lista para armazenar atividades críticas
-    atividades_criticas = []
+    if status_projeto != "Cancelado":
 
-    # Percorre a estrutura do plano de trabalho
-    plano = projeto.get("plano_trabalho", {})
-    componentes = plano.get("componentes", [])
+        # Lista para armazenar atividades críticas
+        atividades_criticas = []
 
-    for comp in componentes:
-        entregas = comp.get("entregas", [])
+        # Percorre a estrutura do plano de trabalho
+        plano = projeto.get("plano_trabalho", {})
+        componentes = plano.get("componentes", [])
 
-        for ent in entregas:
-            atividades = ent.get("atividades", [])
+        for comp in componentes:
+            entregas = comp.get("entregas", [])
 
-            for atv in atividades:
+            for ent in entregas:
+                atividades = ent.get("atividades", [])
 
-                # Calcula o status da atividade usando sua função
-                status = calcular_status_atividade(atv)
+                for atv in atividades:
 
-                # Filtra apenas atividades atrasadas
-                if status in ["atrasada"]:
-                    atividades_criticas.append({
-                        "nome": atv.get("atividade", "Sem nome"),
-                        "status": status
-                    })
+                    # Calcula o status da atividade usando sua função
+                    status = calcular_status_atividade(atv)
 
-    # Exibe a seção apenas se houver atividades críticas
-    if atividades_criticas:
+                    # Filtra apenas atividades atrasadas
+                    if status in ["atrasada"]:
+                        atividades_criticas.append({
+                            "nome": atv.get("atividade", "Sem nome"),
+                            "status": status
+                        })
+
+        # Exibe a seção apenas se houver atividades críticas
+        if atividades_criticas:
 
 
-        # Define singular ou plural automaticamente
-        qtd = len(atividades_criticas)
-        titulo = "atividade atrasada" if qtd == 1 else "atividades atrasadas"
-        st.markdown(f"##### {qtd} {titulo}")
+            # Define singular ou plural automaticamente
+            qtd = len(atividades_criticas)
+            titulo = "atividade atrasada" if qtd == 1 else "atividades atrasadas"
+            st.markdown(f"##### {qtd} {titulo}")
 
-        st.write("Relate o avanço da atividade ou solicite um **remanejamento de prazos** na página de 'Atividades'.")
+            if st.session_state.get("tipo_usuario") == "beneficiario":
+               st.write("Relate o avanço da atividade ou solicite um **remanejamento de prazos** na página de 'Atividades'.")
 
-        for atv in atividades_criticas:
+            for atv in atividades_criticas:
 
-             # Exibe bolinha laranja maior + nome da atividade
-            st.markdown(
-                f"""
-                <span style="display:flex; align-items:center; gap:8px; padding-bottom:10px">
-                    <span style="
-                        width:10px;
-                        height:10px;
-                        background-color:#E2660C;
-                        border-radius:50%;
-                        display:inline-block;
-                        margin-botto:4px;
-                    "></span>
-                    <span>{atv['nome']}</span>
-                </span>
-                """,
-                unsafe_allow_html=True
-            )
+                # Exibe bolinha laranja maior + nome da atividade
+                st.markdown(
+                    f"""
+                    <span style="display:flex; align-items:center; gap:8px; padding-bottom:10px">
+                        <span style="
+                            width:10px;
+                            height:10px;
+                            background-color:#E2660C;
+                            border-radius:50%;
+                            display:inline-block;
+                            margin-botto:4px;
+                        "></span>
+                        <span>{atv['nome']}</span>
+                    </span>
+                    """,
+                    unsafe_allow_html=True
+                )
 
-    st.divider()
+        st.divider()
 
 
 
